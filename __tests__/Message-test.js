d748cf24b6ce0d94a7222275c0eb6df9849cacd5
Gosuhan <david.sylvestre.lp2@gmail.com>
2020-08-12 22:19:22 +0200

    fix beug git

--- __tests__/Message-test.js
+++ __tests__/Message-test.js
@@ -4,7 +4,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Message from '../src/components/Message';
 
-test('renders correctly', () => {
+test('render correctly', () => {
   const tree = renderer.create(<Message />).toJSON();
   expect(tree).toMatchSnapshot();
 });
